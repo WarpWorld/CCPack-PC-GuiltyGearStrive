8f0b120253cf4b811103217080532e29d6d1b809
MrS4turn <67934254+MrS4turn@users.noreply.github.com>
2024-08-30 13:50:35 -0400

    dev sync

--- mod/Mods/CrowdControl/scripts/main.lua
+++ mod/Mods/CrowdControl/scripts/main.lua
@@ -4,190 +4,237 @@ end
 
 function NoPunchButton()
   DisableInput(0x10, 10 * 60)
+  return true
 end
 
 function NoKickButton()
   DisableInput(0x20, 10 * 60)
+  return true
 end
 
 function NoSlashButton()
   DisableInput(0x40, 10 * 60)
+  return true
 end
 
 function NoHeavyButton()
   DisableInput(0x80, 10 * 60)
+  return true
 end
 
 function NoDustButton()
   DisableInput(0x100, 10 * 60)
+  return true
 end
 
 function DisallowAttacks()
   DisableInput(0x1F0, 10 * 60)
+  return true
 end
 
 function ReverseControls()
   ReverseControlsNative(10 * 60)
+  return true
 end
 
 function RandomInputs()
   RandomInputsNative(10 * 60)
+  return true
 end
 
 function Taunt()
   SetInputOverride("Taunt")
+  return true
 end
 
 function WildAssult()
   SetInputOverride("WildAssult")
+  return true
 end
 
 function DeflectShield()
   SetInputOverride("DeflectShield")
+  return true
 end
 
 function Spe236p()
   SetInputOverride("236p")
+  return true
 end
 
 function Spe236k()
   SetInputOverride("236k")
+  return true
 end
 
 function Spe236s()
   SetInputOverride("236s")
+  return true
 end
 
 function Spe236h()
   SetInputOverride("236h")
+  return true
 end
 
 function Spe214p()
   SetInputOverride("214p")
+  return true
 end
 
 function Spe214k()
   SetInputOverride("214k")
+  return true
 end
 
 function Spe214s()
   SetInputOverride("214s")
+  return true
 end
 
 function Spe214h()
   SetInputOverride("214h")
+  return true
 end
 
 function Spe623p()
   SetInputOverride("623p")
+  return true
 end
 
 function Spe623k()
   SetInputOverride("623k")
+  return true
 end
 
 function Spe623s()
   SetInputOverride("623s")
+  return true
 end
 
 function Spe623h()
   SetInputOverride("623h")
+  return true
 end
 
 function Spe41236k()
   SetInputOverride("41236k")
+  return true
 end
 
 function Spe41236h()
   SetInputOverride("41236h")
+  return true
 end
 
 function Spe63214p()
   SetInputOverride("63214p")
+  return true
 end
 
 function Spe63214s()
   SetInputOverride("63214s")
+  return true
 end
 
 function Spe46s()
   SetInputOverride("46s")
+  return true
 end
 
 function Spe46h()
   SetInputOverride("46h")
+  return true
 end
 
 function Spe28s()
   SetInputOverride("28s")
+  return true
 end
 
 function Spe28h()
   SetInputOverride("28h")
+  return true
 end
 
 function Spe2369h()
   SetInputOverride("2369h")
+  return true
 end
 
 function Spe22p()
   SetInputOverride("22p")
+  return true
 end
 
 function Spe22k()
   SetInputOverride("22k")
+  return true
 end
 
 function Spe22s()
   SetInputOverride("22s")
+  return true
 end
 
 function Spe22h()
   SetInputOverride("22h")
+  return true
 end
 
 function BehemothTyphoon()
   SetInputOverride("BehemothTyphoon")
+  return true
 end
 
 function Spe632146p()
   SetInputOverride("632146p")
+  return true
 end
 
 function Spe632146k()
   SetInputOverride("632146k")
+  return true
 end
 
 function Spe632146s()
   SetInputOverride("632146s")
+  return true
 end
 
 function Spe632146h()
   SetInputOverride("632146h")
+  return true
 end
 
 function Spe236236p()
   SetInputOverride("236236p")
+  return true
 end
 
 function Spe236236k()
   SetInputOverride("236236k")
+  return true
 end
 
 function Spe236236s()
   SetInputOverride("236236s")
+  return true
 end
 
 function Spe236236h()
   SetInputOverride("236236h")
+  return true
 end
 
 function Spe236236236h()
   SetInputOverride("236236236h")
+  return true
 end
 
 function Spe214214h()
   SetInputOverride("214214h")
+  return true
 end
 
 function Spe1080p()
@@ -196,6 +243,7 @@ end
 
 function Burst()
   SetInputOverride("Burst")
+  return true
 end
 
 
@@ -231,6 +279,11 @@ function split (inputstr, sep)
 end
 
 
+function string.starts(String,Start)
+  return string.sub(String,1,string.len(Start))==Start
+end
+
+
 LoopAsync(50, function()
 
   if not connected() then return end
@@ -243,6 +296,22 @@ LoopAsync(50, function()
 
   flag = 0x2000
   
+  if string.starts(code, "No") then
+    flag = 0
+  end
+
+  if code == "DisallowAttacks" then
+    flag = 0
+  end
+
+  if code == "ReverseControls" then
+    flag = 0
+  end
+
+  if code == "RandomInputs" then
+    flag = 0
+  end
+
   if code == "Taunt" then
     flag = 0x1000
   end
