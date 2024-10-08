7134443a8fcf20ddb20a80d89952b0993c9b059f
Crowd Control Effect Pack Synchronization Tool <kat@warp.world>
2024-10-08 20:06:37 +0000

    Crowd Control Effect Pack Synchronization Tool - Automated Commit

--- GuiltyGearStrive.cs
+++ GuiltyGearStrive.cs
@@ -1,11 +1,10 @@
-using System;
-using System.Collections.Generic;
+using ConnectorLib.SimpleTCP;
 using CrowdControl.Common;
 using ConnectorType = CrowdControl.Common.ConnectorType;
 
 namespace CrowdControl.Games.Packs.GuiltyGearStrive;
 
-public class GuiltyGearStrive : SimpleTCPPack
+public class GuiltyGearStrive : SimpleTCPPack<SimpleTCPServerConnector>
 {
     public override string Host => "127.0.0.1";
 
