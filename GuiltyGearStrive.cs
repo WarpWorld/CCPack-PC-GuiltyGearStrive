3ba2c70c63621d87e9c1b9925c36d0365646f646
Crowd Control Effect Pack Synchronization Tool <kat@warp.world>
2025-09-22 16:25:17 +0000

    Crowd Control Effect Pack Synchronization Tool - Automated Commit

--- GuiltyGearStrive.cs
+++ GuiltyGearStrive.cs
@@ -11,7 +11,7 @@ public class GuiltyGearStrive : SimpleTCPPack<SimpleTCPServerConnector>
 
     public override ushort Port => 33940;
 
-    [SuppressMessage("PackMetadata", "CC1008:Message Format Property")] //todo - check if this should be changed
+    [SuppressMessage("CrowdControl.PackMetadata", "CC1009:Message Format Property")] //todo - check if this should be changed
     public override ISimpleTCPPack.MessageFormatType MessageFormat => ISimpleTCPPack.MessageFormatType.CrowdControlLegacy;
 
     public override ISimpleTCPPack.QuantityFormatType QuantityFormat => ISimpleTCPPack.QuantityFormatType.ParameterAndField;
